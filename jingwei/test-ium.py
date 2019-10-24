7886142a845142773669c3c6f8b00b29acc287d1
liupengbo <1120691797@qq.com>
2019-10-24 16:16:38 +0800

    药物提醒测试

--- jingwei/test-ium.py
+++ jingwei/test-ium.py
@@ -22,7 +22,7 @@ class __(unittest.TestCase):
         self.dc['appActivity'] = 'com.huidr.HuiDrPatient.activity.SplashActivity'
         self.dc['platformName'] = 'android'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.dc)
-
+    #登录测试
     def test__(self):
         self.driver.find_element_by_xpath("//*[@text='测试']").click()
         self.driver.find_element_by_xpath("//*[@text='立即更新']").click()
@@ -34,6 +34,33 @@ class __(unittest.TestCase):
             '1234')
         self.driver.find_element_by_xpath("//*[@text='获取验证码']").click()
         self.driver.find_element_by_xpath("//*[@text='登录']").click()
+
+    #药物提醒设置测试
+    def test__2(self):
+        self.driver.find_element_by_xpath("//*[@text='测试']").click()
+        self.driver.find_element_by_xpath("//*[@text='立即更新']").click()
+        self.driver.find_element_by_xpath("//*[@text='我的随访']").click()
+        self.driver.find_element_by_xpath(
+            "//*[@text='提醒设置' and ./parent::*[(./preceding-sibling::* | ./following-sibling::*)[@text='随访计划']]]").click()
+        self.driver.find_element_by_xpath("//*[@text='添加提醒']").click()
+        self.driver.find_element_by_xpath("//*[@text='选择药物']").click()
+        self.driver.find_element_by_xpath("//*[@text='雅施达']").click()
+        self.driver.find_element_by_xpath("//*[@text='添加']").click()
+        self.driver.find_element_by_xpath("//*[@text='00:00']").click()
+        self.driver.swipe(270, 2267, 267, 2043, 2383)
+        self.driver.swipe(832, 2127, 810, 1413, 912)
+        self.driver.find_element_by_xpath("//*[@text='确认']").click()
+        self.driver.find_element_by_xpath("//*[@text='完成']").click()
+        self.driver.find_element_by_xpath("//*[@text='每天']").click()
+        self.driver.find_element_by_xpath("//*[@text='每天' and @class='android.widget.Button']").click()
+        self.driver.find_element_by_xpath("//*[@text='确认']").click()
+        self.driver.find_element_by_xpath("//*[@text='单位']").click()
+        self.driver.find_element_by_xpath("//*[@text='克(g)']").click()
+        self.driver.find_element_by_xpath("//*[@text='确认']").click()
+        self.driver.find_element_by_xpath(
+            "//*[@class='android.widget.EditText' and (./preceding-sibling::* | ./following-sibling::*)[@text='剂量']]").send_keys(
+            '5')
+        self.driver.find_element_by_xpath("//*[@text='完成']").click()
 
     def tearDown(self):
         self.driver.quit()
